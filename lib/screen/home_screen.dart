9f14f28666af3d647bb307af3a29f9cbe84036ae
Harvey Yerik <yerik05vh@gmail.com>
2023-01-23 22:50:21 -0500

    styles

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/theme/theme_app.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
    
   const HomeScreen({Key? key}) : super(key: key);
-  
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  void initState(){
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,11 +34,30 @@ class HomeScreen extends StatelessWidget {
             icon: const Icon(Icons.more_vert_sharp),
           ),
         ],
-        bottom: TabBar(
+        bottom: const TabBar(
+          indicatorColor: ThemeApp.white,
+          indicatorWeight: 3.5,
+          labelColor: ThemeApp.white,
           tabs: [
-
+            Tab(
+              child: Icon(Icons.group),
+            ),
+            Tab(
+              child: Text("CHATS"),
+            ),
+            Tab(
+              child: Text("STATUS"),
+            ),
+            Tab(
+              child: Text("CALL"),
+            )
           ]
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: const Icon(Icons.message, color: ThemeApp.white,),
+        
       ),
 
       body: SingleChildScrollView(child: Container(),)
