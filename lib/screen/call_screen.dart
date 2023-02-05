898033d3083da62c9afe5ebd35ac89195e77a6a8
Harvey Yerik <yerik05vh@gmail.com>
2023-02-05 13:45:00 -0500

    Dev: add call screen

--- lib/screen/call_screen.dart
+++ lib/screen/call_screen.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/widgets/widgets.dart';
 
+import '../theme/theme_app.dart';
+
 class CallScreen extends StatelessWidget {
-   
   const CallScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
+
+    final data = ["papa","papa","papa","papa","papa","papa","papa","papa"];
     return SingleChildScrollView(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          LinkWidget()
+          const SizedBox(
+            height: 20.0,
+          ),
+          const LinkWidget(),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal:20.0, vertical: 20.0),
+            child: Text("Recientes",
+                style:
+                    TextStyle(fontWeight: FontWeight.w600, color: ThemeApp.gray)),
+          ),
+          ...data.map((e) => ListTile(
+            
+              title: Text("Juan"),
+              subtitle: Row(
+                children: const [
+                  Icon(Icons.input),
+                  Text("Hace 17 minutos")
+                ],
+              ),
+              leading: const CircleAvatar(
+                radius: 25.0,
+                backgroundImage: NetworkImage("https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+              ),
+              trailing: IconButton(onPressed: (){}, icon: Icon(Icons.call, size: 25.0,color: ThemeApp.green,)),
+              onTap: (){},
+            )
+          )
         ],
       ),
     );
