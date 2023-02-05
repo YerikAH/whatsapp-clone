0df170cdbb1785903b6321416f820a31ed654f0d
Harvey Yerik <yerik05vh@gmail.com>
2023-02-05 15:25:53 -0500

    Dev: add new

--- lib/screen/call_screen.dart
+++ lib/screen/call_screen.dart
@@ -24,12 +24,10 @@ class CallScreen extends StatelessWidget {
     return months[monthNum];
   }
 
-
   CallScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     final dataCalls = data.callsAns;
     return SingleChildScrollView(
       child: Column(
@@ -40,52 +38,68 @@ class CallScreen extends StatelessWidget {
           ),
           const LinkWidget(),
           const Padding(
-            padding: EdgeInsets.symmetric(horizontal:20.0, vertical: 20.0),
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
             child: Text("Recientes",
-                style:
-                    TextStyle(fontWeight: FontWeight.w600, color: ThemeApp.gray)),
+                style: TextStyle(
+                    fontWeight: FontWeight.w600, color: ThemeApp.gray)),
           ),
-          ...dataCalls.map((e){ 
+          ...dataCalls.map((e) {
             DateTime now = DateTime.now();
             int day = now.day;
             int month = now.month;
             String dayFinal;
             String monthFinal = getMonthName(e.lastCallMounth);
-            final String timeString = e.time.substring(0,2);
+            final String timeString = e.time.substring(0, 2);
             final int stringToNumber = int.parse(timeString);
-            final String pmOram =stringToNumber > 12 ? " p.m." : " a.m.";
-            String count = e.countCalls >= 2 ? "(${e.countCalls})" : ""; 
-            if(day == e.lastCallDay){
+            final String pmOram = stringToNumber > 12 ? " p.m." : " a.m.";
+            String count = e.countCalls >= 2 ? "(${e.countCalls})" : "";
+            if (day == e.lastCallDay) {
               dayFinal = "Hoy";
               monthFinal = "";
-            }else if((day - 1 )== e.lastCallDay){
+            } else if ((day - 1) == e.lastCallDay) {
               dayFinal = "Ayer";
               monthFinal = "";
-            }else{
+            } else {
               dayFinal = "$day de";
             }
 
             return ListTile(
-              title: Text(e.name),
+              title: Text(
+                e.name,
+                style: const TextStyle(
+                    color: ThemeApp.black, fontWeight: FontWeight.w500),
+              ),
               subtitle: Row(
                 children: [
                   Transform(
-                    alignment: Alignment.center,
-                    transform: e.answer ?  Matrix4.rotationZ(-0.7) : Matrix4.rotationZ(2.3),
-                    child: Icon(Icons.arrow_forward, color: e.answer ? ThemeApp.greenPale : ThemeApp.red,)
-                  ),
-                  Text("$count $dayFinal $monthFinal ${e.time} $pmOram",overflow: TextOverflow.ellipsis,)
+                      alignment: Alignment.center,
+                      transform: e.answer
+                          ? Matrix4.rotationZ(-0.7)
+                          : Matrix4.rotationZ(2.3),
+                      child: Icon(
+                        Icons.arrow_forward,
+                        color: e.answer ? ThemeApp.greenPale : ThemeApp.red,
+                      )),
+                  Text(
+                    "$count $dayFinal $monthFinal ${e.time} $pmOram",
+                    overflow: TextOverflow.ellipsis,
+                  )
                 ],
               ),
               leading: CircleAvatar(
                 radius: 20.0,
                 backgroundImage: NetworkImage(e.avatar),
               ),
-              trailing: IconButton(onPressed: (){}, icon: const Icon(Icons.call, size: 25.0,color: ThemeApp.green,)),
-              onTap: (){},
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.call,
+                    size: 25.0,
+                    color: ThemeApp.green,
+                  )),
+              onTap: () {},
             );
-          }
-          )
+          })
         ],
       ),
     );
---
+++ lib/screen/status_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StatusScreen extends StatelessWidget {
+   
+  const StatusScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+         child: Text('StatusScreen'),
+      ),
+    );
+  }
+}
